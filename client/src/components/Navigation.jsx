4f8d07d97db6ffd34390280b594a200f73bd9e9a
Khanh <Khanh13Nguyen13@gmail.com>
2024-08-28 18:42:47 +0200

    Routing

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,20 +1,49 @@
 import { NavLink, Outlet } from "react-router-dom";
 
+const checkActive = ({ isActive }) => (isActive ? "is-active" : "");
+
 const Navigation = () => {
 	return (
-		<header>
-			<nav>
-				<NavLink to="/">Home</NavLink>
-				<NavLink to="about-me">About me</NavLink>
-				<NavLink to="my-friends">My Friends</NavLink>
-				<NavLink to="my-portfolio">My Portfolio</NavLink>
-				<NavLink to="join-our-team">Join Our Team</NavLink>
-			</nav>
-
+		<>
+			<header className="flex justify-center mt-8 mb-8">
+				<nav>
+					<li>
+						<NavLink className={`${checkActive} mx-3 text-xl`} to="/">
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className={`${checkActive} mx-3 text-xl`} to="about-me">
+							About me
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className={`${checkActive} mx-3 text-xl`} to="my-friends">
+							My Friends
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className={`${checkActive} mx-3 text-xl`}
+							to="my-portfolio"
+						>
+							My Portfolio
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className={`${checkActive} mx-3 text-xl`}
+							to="join-our-team"
+						>
+							Join Our Team
+						</NavLink>
+					</li>
+				</nav>
+			</header>
 			<div className="container px-14">
 				<Outlet />
 			</div>
-		</header>
+		</>
 	);
 };
 
