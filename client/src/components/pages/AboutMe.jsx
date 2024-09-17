614a5d83be0257100cebc60d545342b26a14f3b4
Khanh <Khanh13Nguyen13@gmail.com>
2024-09-17 21:46:41 +0200

    Start aboute me page

--- client/src/components/pages/AboutMe.jsx
+++ client/src/components/pages/AboutMe.jsx
@@ -1,5 +1,38 @@
 const AboutMe = () => {
-	return <div>AboutMe</div>;
+	return (
+		<div>
+			<img src="./img/me.jpg" alt="my photo" className="w-2/4 mx-auto mb-6" />
+			<h1 className="font-semibold text-4xl italic text-center mb-5">
+				My name is{" "}
+				<span className="font-light italic text-indigo-600">
+					Leonardo Wilhelm DiCaprio
+				</span>
+			</h1>
+			<p className="max-w-screen-lg mx-auto text-center text-2xl">
+				Leonardo Wilhelm DiCaprio (/diˈkæprioʊ, dɪ-/; Italian: [diˈkaːprjo];
+				born November 11, 1974) is an American actor and film producer. Known
+				for his work in biographical and period films, he is the recipient of
+				numerous accolades, including an Academy Award, a British Academy Film
+				Award, and three Golden Globe Awards. As of 2019, his films have grossed
+				over $7.2 billion worldwide, and he has been placed eight times in
+				annual rankings of the world's highest-paid actors. Born in Los Angeles,
+				DiCaprio began his career in the late 1980s by appearing in television
+				commercials. In the early 1990s, he had recurring roles in various
+				television shows, such as the sitcom Parenthood, and had his first major
+				film part as author Tobias Wolff in This Boy's Life (1993). He received
+				critical acclaim and his first Academy Award and Golden Globe Award
+				nominations for his performance as a developmentally disabled boy in
+				What's Eating Gilbert Grape (1993). DiCaprio achieved international
+				stardom with the star-crossed romances Romeo + Juliet (1996) and Titanic
+				(1997). After the latter became the highest-grossing film in the world
+				at the time, he reduced his workload for a few years. In an attempt to
+				shed his image of a romantic hero, DiCaprio sought roles in other
+				genres, including the 2002 crime dramas Catch Me If You Can and Gangs of
+				New York; the latter marked the first of his many successful
+				collaborations with director Martin Scorsese.
+			</p>
+		</div>
+	);
 };
 
 export default AboutMe;
